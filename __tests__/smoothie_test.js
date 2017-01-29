5c51970b4252a51c1a3a7c0de54a1d4e59ef7cbc
Jaap Frolich <jfrolich@gmail.com>
2017-01-29 22:47:28 +0800

    fix bug with special characters see issue #10

--- __tests__/smoothie_test.js
+++ __tests__/smoothie_test.js
@@ -2,87 +2,110 @@ const exec = require('child_process').exec;
 const path = require('path');
 const fs = require('fs');
 
-const readFile = (filename) => fs.readFileSync(filename, { encoding: 'utf-8' })
-const snapshot = (filename) => readFile(path.join('__tests__', 'data', 'snapshots', filename));
-const build = (templateDir, filename) => readFile(path.join('__tests__', 'data', templateDir, 'build', filename));
+const readFile = filename =>
+  fs.readFileSync(filename, { encoding: 'utf-8' }).replace(/^\s+|\s+$/g, '');
+const snapshot = filename =>
+  readFile(path.join('__tests__', 'data', 'snapshots', filename));
+const build = (templateDir, filename) =>
+  readFile(path.join('__tests__', 'data', templateDir, 'build', filename));
 
 PROCESS_ENV = process.env;
 
 const runScript = (opts = {}) => new Promise((resolve, reject) => {
-  exec("./index.js", opts, (error, stdout, stderr) => {
-    // console.error(stderr)
-    if (error) reject(error)
-    else resolve({stdout, stderr});
-  })
-})
-
-test('expect to fail when no template dir specified', () => {
-  return runScript().then(({stderr}) => expect(stderr).toBe('Error: No template dir specified\n'))
+  exec('./index.js', opts, (error, stdout, stderr) => {
+    console.error(stdout);
+    console.error(stderr);
+    if (error) reject(error);
+    else resolve({ stdout, stderr });
+  });
 });
 
-test('handle templates without layouts', () =>
-  runScript({env: Object.assign(PROCESS_ENV, {
-    SMOOTHIE_TEMPLATE_DIR: '__tests__/data/simple'
-  })})
-  .then(() => {
-    const html = 'template_simple.html.eex';
-    const txt = 'template_simple.txt.eex';
-    expect(build('simple', html)).toEqual(snapshot(html));
-    expect(build('simple', txt)).toEqual(snapshot(txt));
-  })
-)
+test('expect to fail when no template dir specified', () => {
+  return runScript().then(
+    ({ stderr }) => expect(stderr).toBe('Error: No template dir specified\n')
+  );
+});
 
-test('handle templates with foundation', () =>
-  runScript({env: Object.assign(PROCESS_ENV, {
-    SMOOTHIE_TEMPLATE_DIR: '__tests__/data/simple_with_foundation',
-    SMOOTHIE_USE_FOUNDATION: 'true'
-  })})
-  .then(() => {
-    const html = 'template_simple_with_foundation.html.eex';
-    const txt = 'template_simple_with_foundation.txt.eex';
-    expect(build('simple_with_foundation', html)).toEqual(snapshot(html));
-    expect(build('simple_with_foundation', txt)).toEqual(snapshot(txt));
-  })
-)
+test(
+  'handle templates without layouts',
+  () =>
+    runScript({
+      env: Object.assign(PROCESS_ENV, {
+        SMOOTHIE_TEMPLATE_DIR: '__tests__/data/simple'
+      })
+    }).then(() => {
+      const html = 'template_simple.html.eex';
+      const txt = 'template_simple.txt.eex';
+      expect(build('simple', html)).toEqual(snapshot(html));
+      expect(build('simple', txt)).toEqual(snapshot(txt));
+    })
+);
 
-test('handle layout file not found', () =>
-  runScript({env: Object.assign(PROCESS_ENV, {
-    SMOOTHIE_TEMPLATE_DIR: '__tests__/data/simple',
-    SMOOTHIE_LAYOUT_FILE:  '__tests__/data/layout/wrong.html.eex',
-    SMOOTHIE_USE_FOUNDATION: 'false'
-  })})
-  .then(({stderr}) => {
-    expect(stderr).toBe('Error: Layout file not found\n');
-  })
-)
+test(
+  'handle templates with foundation',
+  () =>
+    runScript({
+      env: Object.assign(PROCESS_ENV, {
+        SMOOTHIE_TEMPLATE_DIR: '__tests__/data/simple_with_foundation',
+        SMOOTHIE_USE_FOUNDATION: 'true'
+      })
+    }).then(() => {
+      const html = 'template_simple_with_foundation.html.eex';
+      const txt = 'template_simple_with_foundation.txt.eex';
+      expect(build('simple_with_foundation', html)).toEqual(snapshot(html));
+      expect(build('simple_with_foundation', txt)).toEqual(snapshot(txt));
+    })
+);
 
+test(
+  'handle layout file not found',
+  () =>
+    runScript({
+      env: Object.assign(PROCESS_ENV, {
+        SMOOTHIE_TEMPLATE_DIR: '__tests__/data/simple',
+        SMOOTHIE_LAYOUT_FILE: '__tests__/data/layout/wrong.html.eex',
+        SMOOTHIE_USE_FOUNDATION: 'false'
+      })
+    }).then(({ stderr }) => {
+      expect(stderr).toBe('Error: Layout file not found\n');
+    })
+);
 
-test('handle templates with layouts', () =>
-  runScript({env: Object.assign(PROCESS_ENV, {
-    SMOOTHIE_TEMPLATE_DIR: '__tests__/data/simple_with_layout',
-    SMOOTHIE_LAYOUT_FILE:  '__tests__/data/layout/layout.html.eex',
-    SMOOTHIE_USE_FOUNDATION: 'false'
+test(
+  'handle templates with layouts',
+  () =>
+    runScript({
+      env: Object.assign(PROCESS_ENV, {
+        SMOOTHIE_TEMPLATE_DIR: '__tests__/data/simple_with_layout',
+        SMOOTHIE_LAYOUT_FILE: '__tests__/data/layout/layout.html.eex',
+        SMOOTHIE_USE_FOUNDATION: 'false'
+      })
+    }).then(() => {
+      const html = 'template_simple_with_layout.html.eex';
+      const txt = 'template_simple_with_layout.txt.eex';
+      expect(build('simple_with_layout', html)).toEqual(snapshot(html));
+      expect(build('simple_with_layout', txt)).toEqual(snapshot(txt));
+    })
+);
 
-  })})
-  .then(() => {
-    const html = 'template_simple_with_layout.html.eex';
-    const txt = 'template_simple_with_layout.txt.eex';
-    expect(build('simple_with_layout', html)).toEqual(snapshot(html));
-    expect(build('simple_with_layout',txt)).toEqual(snapshot(txt));
-  })
-)
-
-test('handle templates with layouts and sass', () =>
-  runScript({env: Object.assign(PROCESS_ENV, {
-    SMOOTHIE_TEMPLATE_DIR: '__tests__/data/simple_with_layout_and_stylesheet',
-    SMOOTHIE_LAYOUT_FILE:  '__tests__/data/layout/layout.html.eex',
-    SMOOTHIE_SCSS_FILE: '__tests__/data/css/style.scss',
-    SMOOTHIE_USE_FOUNDATION: 'false'
-  })})
-  .then(({stdout}) => {
-    const html = 'template_simple_with_layout_and_stylesheet.html.eex';
-    const txt = 'template_simple_with_layout_and_stylesheet.txt.eex';
-    expect(build('simple_with_layout_and_stylesheet', html)).toEqual(snapshot(html));
-    expect(build('simple_with_layout_and_stylesheet', txt)).toEqual(snapshot(txt));
-  })
-)
+test(
+  'handle templates with layouts and sass',
+  () =>
+    runScript({
+      env: Object.assign(PROCESS_ENV, {
+        SMOOTHIE_TEMPLATE_DIR: '__tests__/data/simple_with_layout_and_stylesheet',
+        SMOOTHIE_LAYOUT_FILE: '__tests__/data/layout/layout.html.eex',
+        SMOOTHIE_SCSS_FILE: '__tests__/data/css/style.scss',
+        SMOOTHIE_USE_FOUNDATION: 'false'
+      })
+    }).then(({ stdout }) => {
+      const html = 'template_simple_with_layout_and_stylesheet.html.eex';
+      const txt = 'template_simple_with_layout_and_stylesheet.txt.eex';
+      expect(
+        build('simple_with_layout_and_stylesheet', html)
+      ).toEqual(snapshot(html));
+      expect(
+        build('simple_with_layout_and_stylesheet', txt)
+      ).toEqual(snapshot(txt));
+    })
+);
--- index.js
+++ index.js
@@ -10,10 +10,26 @@ const cheerio = require('cheerio');
 const chalk = require('chalk');
 
 const TEMPLATE_DIR = process.env.SMOOTHIE_TEMPLATE_DIR;
-const LAYOUT_FILE = process.env.SMOOTHIE_LAYOUT_FILE ? process.env.SMOOTHIE_LAYOUT_FILE : undefined;
-const CSS_FILE = process.env.SMOOTHIE_CSS_FILE ? process.env.SMOOTHIE_CSS_FILE : undefined;
-const SCSS_FILE = process.env.SMOOTHIE_SCSS_FILE ? process.env.SMOOTHIE_SCSS_FILE : undefined;
-const USE_FOUNDATION = process.env.SMOOTHIE_USE_FOUNDATION ? (process.env.SMOOTHIE_USE_FOUNDATION == 'true') : false;
+const LAYOUT_FILE = process.env.SMOOTHIE_LAYOUT_FILE
+  ? process.env.SMOOTHIE_LAYOUT_FILE
+  : undefined;
+const CSS_FILE = process.env.SMOOTHIE_CSS_FILE
+  ? process.env.SMOOTHIE_CSS_FILE
+  : undefined;
+const SCSS_FILE = process.env.SMOOTHIE_SCSS_FILE
+  ? process.env.SMOOTHIE_SCSS_FILE
+  : undefined;
+const USE_FOUNDATION = process.env.SMOOTHIE_USE_FOUNDATION
+  ? process.env.SMOOTHIE_USE_FOUNDATION == 'true'
+  : false;
+
+const btoa = string => {
+  return new Buffer(string).toString('base64');
+};
+
+const atob = string => {
+  return Buffer(string, 'base64').toString('ascii');
+};
 
 class SmoothieException {
   constructor(message) {
@@ -22,85 +38,97 @@ class SmoothieException {
 }
 
 try {
-  if (!TEMPLATE_DIR) throw(new SmoothieException("No template dir specified"))
+  if (!TEMPLATE_DIR) throw new SmoothieException('No template dir specified');
   // fs.accessSync(TEMPLATE_DIR, fs.F_OK);
-
-  const FOUNDATION_STYLE_PATH = path.join('node_modules/foundation-emails/dist', 'foundation-emails.css');
+  const FOUNDATION_STYLE_PATH = path.join(
+    'node_modules/foundation-emails/dist',
+    'foundation-emails.css'
+  );
 
   console.log('Preparing to compile the following template files:');
-  const templateFiles = fs.readdirSync(TEMPLATE_DIR).filter(file => file.includes('.eex'));
-  console.log(templateFiles.map(file => '- ' + file).join('\n'))
+  const templateFiles = fs
+    .readdirSync(TEMPLATE_DIR)
+    .filter(file => file.includes('.eex'));
+  console.log(templateFiles.map(file => '- ' + file).join('\n'));
 
-  let css = USE_FOUNDATION ? fs.readFileSync(FOUNDATION_STYLE_PATH, 'utf8') : '';
+  let css = USE_FOUNDATION
+    ? fs.readFileSync(FOUNDATION_STYLE_PATH, 'utf8')
+    : '';
 
   if (SCSS_FILE) {
-    css += sass.renderSync({ file: SCSS_FILE }).css
-  } else if(CSS_FILE) {
+    css += sass.renderSync({ file: SCSS_FILE }).css;
+  } else if (CSS_FILE) {
     css += fs.readFileSync(CSS_FILE, 'utf8');
   }
 
   const JUICE_OPTIONS = {
-      preserveImportant: true,
-      removeStyleTags: true,
-      extraCss: css,
+    preserveImportant: true,
+    removeStyleTags: true,
+    extraCss: css
   };
 
   let layout = '';
   try {
     layout = LAYOUT_FILE ? fs.readFileSync(LAYOUT_FILE, 'utf8') : '{content}';
   } catch (e) {
-    if ( e.code != 'EEXIST' ) throw new SmoothieException('Layout file not found');
-    else throw(e)
+    if (e.code != 'EEXIST')
+      throw new SmoothieException('Layout file not found');
+    else
+      throw e;
   }
 
   if (LAYOUT_FILE) console.log(`Using layout file: ${LAYOUT_FILE}`);
   else console.log('No layout specified');
 
-  const escapeEex = text => text.replace(/<%(.*?)%>/g, (match, p1) => `{{{${p1}}}}`);
-  const unescapeEex = text => text.replace(/{{{(.*?)}}}/g, (match, p1) => `<%${p1}%>`);
-  const compileInky = $ => (new Inky({})).releaseTheKraken($)
+  const escapeEex = text =>
+    text.replace(/<%(.*?)%>/g, (match, p1) => `{{{${btoa(p1)}}}}`);
+  const unescapeEex = text =>
+    text.replace(/{{{(.*?)}}}/g, (match, p1) => `<%${atob(p1)}%>`);
+  const compileInky = $ => new Inky({}).releaseTheKraken($);
 
   templateFiles.forEach(file => {
-    let template = fs.readFileSync(path.join(TEMPLATE_DIR, file), { encoding: 'utf-8' });
+    let template = fs.readFileSync(path.join(TEMPLATE_DIR, file), {
+      encoding: 'utf-8'
+    });
     template = layout.replace('{content}', template);
 
-    let $ = cheerio.load(escapeEex(template), {xmlMode: true});
+    let $ = cheerio.load(escapeEex(template), { xmlMode: true });
     // const templateCss = $('style').contents().toArray().reduce((prev, curr) => prev + curr.data, '');
     // do not remove inline styles, they could contain things like media queries
     // $('style').remove();
-
     template = USE_FOUNDATION ? compileInky($) : $.html();
     template = unescapeEex(template);
 
     const juicedTemplate = juice(template, JUICE_OPTIONS);
-    const textTemplate = unescapeEex(htmlToText.fromString(escapeEex(template), {
-      uppercaseHeadings: false,
-    }));
-
-    try { fs.mkdirSync(path.join(TEMPLATE_DIR, 'build')) }
-    catch(e) {
-      if ( e.code != 'EEXIST' ) throw e;
-    }
-
-    fs.writeFileSync(
-      path.join(TEMPLATE_DIR, 'build', file),
-      juicedTemplate
+    const textTemplate = unescapeEex(
+      htmlToText.fromString(escapeEex(template), { uppercaseHeadings: false })
     );
 
-    console.log('Created ' + file)
+    try {
+      fs.mkdirSync(path.join(TEMPLATE_DIR, 'build'));
+    } catch (e) {
+      if (e.code != 'EEXIST') throw e;
+    }
+
+    fs.writeFileSync(path.join(TEMPLATE_DIR, 'build', file), juicedTemplate);
+
+    console.log('Created ' + file);
 
     fs.writeFileSync(
       path.join(TEMPLATE_DIR, 'build', file.replace('html.eex', 'txt.eex')),
       textTemplate
     );
 
-    console.log('Created ' + file.replace('html.eex', 'txt.eex'))
-  })
-  console.log('Done 🙏');
-} catch(error) {
+    console.log('Created ' + file.replace('html.eex', 'txt.eex'));
+  });
+  console.log('Done \uD83D\uDE4F');
+} catch (error) {
   if (error instanceof SmoothieException) {
-    console.error(chalk.bold(chalk.underline(chalk.red('Error:'))), error.message)
+    console.error(
+      chalk.bold(chalk.underline(chalk.red('Error:'))),
+      error.message
+    );
   } else {
-    throw(error)
+    throw error;
   }
 }
